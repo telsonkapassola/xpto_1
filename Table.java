9f71792ec65ecd17557c556458e10527078ca250
telsonaldair <telsonkapassola@hotmail.com>
2019-08-12 10:18:47 +0100

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Formação
+ */
+public class Table {
+    
+    private int num[] = new int[4];
+    
+    Scanner in = new Scanner(System.in);
+    
+    public Table(){
+        System.out.println("Insira os valores para retornar os diferentes formatos: \n");
+        for(int i = 0; i<num.length; i++ ){
+            num[i] = in.nextInt();
+        }
+    }
+    
+    public void print(Printer printer){
+        for(int i = 0; i<num.length; i++ ){
+            System.out.println(printer.format(num[i]));
+        }
+    }
+}
