7de5bbf57e2f0715f919aa9795f75f7b29b3271b
Telson Kapassola <telsonkapassola@hotmail.com>
2019-08-17 17:11:23 +0100

    Atualização do vetor para [2]

--- Table.java
+++ Table.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,7 +12,7 @@ import java.util.Scanner;
  */
 public class Table {
     
-    private int num[] = new int[4];
+    private int num[] = new int[2];
     
     Scanner in = new Scanner(System.in);
     
